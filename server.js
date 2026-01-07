f59acd4ea2475652021280cd829765f70d60a1ce
Swadesh-c0de <patelswadesh7@gmail.com>
2026-01-07 18:12:15 +0530

    adding projects files

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import 'dotenv/config';
+import contactRoutes from './routes/contactRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import errorHandler from './middleware/errorHandler.js';
+import connectDB from './config/dbConnection.js';
+
+connectDB();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(errorHandler);
+app.use('/api/contacts', contactRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}.`);
+})
